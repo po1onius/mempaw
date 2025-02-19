accdeea8d37389c1182b88384450b80098d0ed1e
srus <po1onius@outlook.com>
2025-02-19 23:01:49 +0800

    dev ing

--- src/memdata.rs
+++ src/memdata.rs
@@ -2,6 +2,7 @@ use ordered_float::OrderedFloat;
 use serde::{Deserialize, Serialize};
 use std::{
     collections::{HashMap, HashSet, LinkedList},
+    default,
     fs::File,
     io::Write,
 };
@@ -9,7 +10,7 @@ use std::{
 use crate::config::get_config;
 
 #[derive(Deserialize, Serialize)]
-enum InerWrapValue {
+enum InnerWrapValue {
     Str(String),
     Set(HashSet<String>),
     Hash(HashMap<String, String>),
@@ -17,9 +18,21 @@ enum InerWrapValue {
     Zset(HashMap<OrderedFloat<f32>, String>),
 }
 
+#[derive(Deserialize, Serialize, Default)]
+struct KeyMeta {
+    time_stamp: u32,
+    access_times: u32,
+}
+
+#[derive(Deserialize, Serialize)]
+struct CoreVaule {
+    meta: KeyMeta,
+    value: InnerWrapValue,
+}
+
 #[derive(Deserialize, Serialize)]
 pub struct CoreData {
-    data: HashMap<String, InerWrapValue>,
+    data: HashMap<String, CoreVaule>,
 }
 
 impl CoreData {
@@ -32,5 +45,37 @@ impl CoreData {
         Self {
             data: HashMap::new(),
         }
+    }
+
+    pub fn del(&mut self, k: &str) {
+        self.data.remove(k);
+    }
+
+    pub fn set(&mut self, k: &str, v: &str) {
+        let r = self.data.insert(
+            k.to_string(),
+            CoreVaule {
+                value: InnerWrapValue::Str(v.to_string()),
+                meta: KeyMeta::default(),
+            },
+        );
+    }
+
+    pub fn get(&mut self, k: &str, v: &str) -> String {
+        if let Some(inner) = self.data.get_mut(k) {
+            if let CoreVaule {
+                meta:
+                    KeyMeta {
+                        time_stamp: _,
+                        access_times,
+                    },
+                value: InnerWrapValue::Str(s),
+            } = inner
+            {
+                *access_times += 1;
+                return s.to_string();
+            }
+        }
+        "".into()
     }
 }
