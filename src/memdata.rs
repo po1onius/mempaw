4a3c39d73ec6c000d2c7e2c42ac8e603f6546208
srus <po1onius@outlook.com>
2025-02-25 01:04:07 +0800

    move key meta to value (dev ing)

--- src/memdata.rs
+++ src/memdata.rs
@@ -4,7 +4,6 @@ use std::{
     cell::Cell,
     collections::{HashMap, HashSet, LinkedList},
     fs::{File, OpenOptions},
-    hash::Hash,
     io::{Read, Write},
 };
 
@@ -19,39 +18,28 @@ enum InnerWrapValue {
     Zset(HashMap<OrderedFloat<f32>, String>),
 }
 
-#[derive(BorshDeserialize, BorshSerialize, Default, PartialOrd, Ord, Eq)]
-struct CoreKey {
-    key: String,
+#[derive(BorshDeserialize, BorshSerialize, Default)]
+struct CoreKeyMeta {
     time_stamp: Cell<u32>,
     access_times: Cell<u32>,
 }
 
+#[derive(BorshDeserialize, BorshSerialize)]
+struct CoreValue {
+    meta: CoreKeyMeta,
+    value: InnerWrapValue,
+}
+
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct CoreData {
-    data: HashMap<CoreKey, InnerWrapValue>,
+    data: HashMap<String, CoreValue>,
 }
 
 pub fn test() {}
 
-impl PartialEq for CoreKey {
-    fn eq(&self, other: &Self) -> bool {
-        self.key == other.key
-    }
-}
-
-impl Hash for CoreKey {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.key.hash(state);
-    }
-}
-
-impl From<&str> for CoreKey {
-    fn from(value: &str) -> Self {
-        CoreKey {
-            key: value.to_string(),
-            time_stamp: Cell::new(0),
-            access_times: Cell::new(0),
-        }
+impl CoreValue {
+    fn access(&self) {
+        self.meta.access_times.set(self.meta.access_times.get() + 1);
     }
 }
 
@@ -73,55 +61,56 @@ impl CoreData {
     }
 
     pub fn del(&mut self, k: &str) {
-        self.data.remove(&CoreKey::from(k));
+        self.data.remove(k).unwrap();
     }
 
     pub fn set(&mut self, k: &str, v: &str) {
-        let r = self.data.insert(
-            CoreKey {
-                key: k.to_string(),
-                time_stamp: Cell::new(0),
-                access_times: Cell::new(0),
+        match self.data.get_mut(k) {
+            Some(inner) => match &mut inner.value {
+                InnerWrapValue::Str(s) => {
+                    *s = v.to_string();
+                }
+                _ => (),
             },
-            InnerWrapValue::Str(v.to_string()),
-        );
-    }
-
-    pub fn get(&mut self, k: &str, v: &str) -> Result<String, ()> {
-        if let Some(inner) = self.data.get_mut(&CoreKey::from(k)) {
-            if let CoreVaule {
-                meta:
-                    KeyMeta {
-                        time_stamp: _,
-                        access_times,
+            None => {
+                self.data.insert(
+                    k.to_string(),
+                    CoreValue {
+                        meta: CoreKeyMeta::default(),
+                        value: InnerWrapValue::Str(v.to_string()),
                     },
-                value: InnerWrapValue::Str(s),
-            } = inner
-            {
-                *access_times += 1;
-                return Ok(s.to_string());
+                );
             }
         }
-        Err(())
+    }
+
+    pub fn get(&self, k: &str) -> Result<&str, ()> {
+        if let Some(CoreValue {
+            meta:
+                CoreKeyMeta {
+                    time_stamp: _,
+                    access_times,
+                },
+            value: InnerWrapValue::Str(s),
+        }) = self.data.get(k)
+        {
+            access_times.set(access_times.get() + 1);
+            Ok(s)
+        } else {
+            Err(())
+        }
     }
 
     pub fn hdel(&mut self, k: &str, kk: &str) -> Result<(), ()> {
-        if let Some(inner) = self.data.get_mut(k) {
-            if let CoreVaule {
-                meta:
-                    KeyMeta {
-                        time_stamp: _,
-                        access_times,
-                    },
-                value: InnerWrapValue::Hash(hm),
-            } = inner
-            {
-                *access_times += 1;
-                if hm.remove(kk).is_some() {
-                    return Ok(());
-                }
+        if let Some(CoreValue {
+            meta: _,
+            value: InnerWrapValue::Hash(m),
+        }) = self.data.get_mut(k)
+        {
+            if m.remove(kk).is_some() {
+                return Ok(());
             }
         }
-        return Err(());
+        Err(())
     }
 }
